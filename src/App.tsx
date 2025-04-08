b0f1335de99b3e39e1893b4acc5e158601a3b23b
Shingo Azuma <s.azuma@melta.co.jp>
2025-04-08 18:48:12 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Scene from './components/Scene'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <Scene />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Scene.tsx
@@ -0,0 +1,71 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Grid } from '@react-three/drei'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { Plane, Vector3, Mesh } from 'three'
+import { useRef } from 'react'
+
+const Sphere = () => {
+  const meshRef = useRef<Mesh>(null)
+  const clippingPlane = new Plane()
+  clippingPlane.setFromNormalAndCoplanarPoint(
+    new Vector3(1, 0, 0), // 法線ベクトル (X軸に垂直)
+    new Vector3(0, 0, 0)  // 平面上の点
+  )
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += delta * 0.5 // ゆっくり回転（0.5は回転速度）
+    }
+  })
+
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial 
+        color="#ffffff" 
+        emissive="#ffffff" 
+        emissiveIntensity={0.5}
+        clippingPlanes={[clippingPlane]}
+        clipShadows={true}
+      />
+    </mesh>
+  )
+}
+
+const Scene = () => {
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 5], fov: 75 }}
+      style={{ width: '100vw', height: '100vh', background: '#000000' }}
+      gl={{ localClippingEnabled: true }}
+    >
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Grid
+        args={[10, 10]}
+        cellSize={1}
+        cellThickness={1}
+        cellColor="#6f6f6f"
+        sectionSize={3.3}
+        sectionThickness={1.5}
+        sectionColor="#9d4b4b"
+        fadeDistance={30}
+        fadeStrength={1}
+        followCamera={false}
+        infiniteGrid={true}
+      />
+      <Sphere />
+      <OrbitControls enableDamping dampingFactor={0.05} />
+      <EffectComposer>
+        <Bloom
+          intensity={1.0}
+          luminanceThreshold={0.1}
+          luminanceSmoothing={0.9}
+          height={300}
+        />
+      </EffectComposer>
+    </Canvas>
+  )
+}
+
+export default Scene 
